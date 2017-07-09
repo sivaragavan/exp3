7845593687668565fccdbad315893bfeb3c50b89
Sivaprakash Ragavan <mail@sivragav.com>
2017-07-08 22:29:45 -0400

    Updating exp number

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
           <div className="col-md-12">
             <div className="panel panel-default flex-col">
-              <div className="panel-heading">Experiment 1 - A Text Editor with a built-in assistant</div>
+              <div className="panel-heading">Experiment 3 - Rich Text Editor with a Comment Bot</div>
               <div className="panel-body flex-grow">
                 <textarea
                   ref={(input) => { this.input = input; }}
